c5fa01f08ee211601f1317d27042f2b55c97b0d7
L.M.A.H. Premawansha <78247019+aselahemantha@users.noreply.github.com>
2023-09-26 23:32:19 +0530

    fix error in LCD

--- Quartus Project/ic_tester/PFCount.v
+++ Quartus Project/ic_tester/PFCount.v
@@ -9,134 +9,153 @@ module PFCount(
 
     always @(*) begin
 	 
-		  if (icg == 1'b1) begin
-			  
-			  if (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) PASS = 4'b0110;
-			  
-			  if ((pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) || 
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1)) PASS = 4'b0101;
-			  
-			  if ((pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			  (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
-			  (pass1 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1)) PASS = 4'b0100;
-	  
-			  
-			  if ((pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			 (pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			 (pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			 (pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
-			 (pass2 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			 (pass2 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			 (pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
-			 (pass2 == 1'b1 && pass3 == 1'b1 && pass6 == 1'b1) ||
-			 (pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1) ||
-			 (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1) ||
-			 (pass1 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
-			 (pass1 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
-			 (pass1 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
-			 (pass1 == 1'b1 && pass3 == 1'b1 && pass6 == 1'b1) ||
-			 (pass1 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1) ||
-			 (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1)) PASS = 4'b0011;
-			  
-			  if ((pass5 == 1'b1 && pass6 == 1'b1) ||
-			  (pass4 == 1'b1 && pass6 == 1'b1) ||
-			  (pass4 == 1'b1 && pass5 == 1'b1) ||
-			  (pass3 == 1'b1 && pass6 == 1'b1) ||
-			  (pass3 == 1'b1 && pass5 == 1'b1) ||
-			  (pass3 == 1'b1 && pass4 == 1'b1) ||
-			  (pass2 == 1'b1 && pass6 == 1'b1) ||
-			  (pass2 == 1'b1 && pass5 == 1'b1) ||
-			  (pass2 == 1'b1 && pass4 == 1'b1) ||
-			  (pass2 == 1'b1 && pass3 == 1'b1) ||
-			  (pass1 == 1'b1 && pass6 == 1'b1) ||
-			  (pass1 == 1'b1 && pass5 == 1'b1) ||
-			  (pass1 == 1'b1 && pass4 == 1'b1) ||
-			  (pass1 == 1'b1 && pass3 == 1'b1) ||
-			  (pass1 == 1'b1 && pass2 == 1'b1)) PASS = 4'b0010;
-			  
-			  if (pass1 == 1'b1) PASS = 4'b0001;
-			  
+		  if (icg == 1'b1) begin 
+
+			  if (pass1 == 1'b1 || pass2 == 1'b1 || pass3 == 1'b1 || pass2 == 1'b1 || pass2 == 1'b1 || pass2 == 1'b1) begin
+					PASS = 4'b0001;		  
+				  
+				   if ((pass5 == 1'b1 && pass6 == 1'b1) ||
+				   (pass4 == 1'b1 && pass6 == 1'b1) ||
+				   (pass4 == 1'b1 && pass5 == 1'b1) ||
+				   (pass3 == 1'b1 && pass6 == 1'b1) ||
+				   (pass3 == 1'b1 && pass5 == 1'b1) ||
+				   (pass3 == 1'b1 && pass4 == 1'b1) ||
+				   (pass2 == 1'b1 && pass6 == 1'b1) ||
+				   (pass2 == 1'b1 && pass5 == 1'b1) ||
+				   (pass2 == 1'b1 && pass4 == 1'b1) ||
+				   (pass2 == 1'b1 && pass3 == 1'b1) ||
+				   (pass1 == 1'b1 && pass6 == 1'b1) ||
+				   (pass1 == 1'b1 && pass5 == 1'b1) ||
+				   (pass1 == 1'b1 && pass4 == 1'b1) ||
+				   (pass1 == 1'b1 && pass3 == 1'b1) ||
+				   (pass1 == 1'b1 && pass2 == 1'b1)) begin 
+						PASS = 4'b0010;
+						
+					   if ((pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+					   (pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+					   (pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+						(pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
+						(pass2 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+						(pass2 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+						(pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
+						(pass2 == 1'b1 && pass3 == 1'b1 && pass6 == 1'b1) ||
+						(pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1) ||
+						(pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1) ||
+						(pass1 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+						(pass1 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+						(pass1 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
+						(pass1 == 1'b1 && pass3 == 1'b1 && pass6 == 1'b1) ||
+						(pass1 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1) ||
+						(pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1)) begin 
+							PASS = 4'b0011;
+							
+							if ((pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+							(pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+							(pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+						   (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+						   (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
+						   (pass1 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+						   (pass1 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+						   (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+						   (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
+						   (pass1 == 1'b1 && pass2 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+						   (pass1 == 1'b1 && pass2 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+						   (pass1 == 1'b1 && pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) ||
+						   (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass6 == 1'b1) ||
+						   (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1) ||
+						   (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1)) begin 
+								PASS = 4'b0100;
+											  
+							  if ((pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1) || 
+							  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass6 == 1'b1) ||
+							  (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+							  (pass1 == 1'b1 && pass2 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+							  (pass1 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) ||
+							  (pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1)) begin 
+									PASS = 4'b0101;
+									
+									if (pass1 == 1'b1 && pass2 == 1'b1 && pass3 == 1'b1 && pass4 == 1'b1 && pass5 == 1'b1 && pass6 == 1'b1) begin 
+										PASS = 4'b0110;
+									end
+								end
+							end
+						end	
+				  end
+			  end
 			  // Fail
 				 
-			  if (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) FAIL = 4'b0110;
-			  
-			  if ((fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) || 
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1)) FAIL = 4'b0101;
-
-			  
-			  if ((fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1)) FAIL = 4'b0100;
-
-			  
-			  if ((fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail2 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1) ||
-			  (fail1 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1)) FAIL = 4'b0011;
-
-			  
-			  if ((fail5 == 1'b1 && fail6 == 1'b1) ||
-			  (fail4 == 1'b1 && fail6 == 1'b1) ||
-			  (fail4 == 1'b1 && fail5 == 1'b1) ||
-			  (fail3 == 1'b1 && fail6 == 1'b1) ||
-			  (fail3 == 1'b1 && fail5 == 1'b1) ||
-			  (fail3 == 1'b1 && fail4 == 1'b1) ||
-			  (fail2 == 1'b1 && fail6 == 1'b1) ||
-			  (fail2 == 1'b1 && fail5 == 1'b1) ||
-			  (fail2 == 1'b1 && fail4 == 1'b1) ||
-			  (fail2 == 1'b1 && fail3 == 1'b1) ||
-			  (fail1 == 1'b1 && fail6 == 1'b1) ||
-			  (fail1 == 1'b1 && fail5 == 1'b1) ||
-			  (fail1 == 1'b1 && fail4 == 1'b1) ||
-			  (fail1 == 1'b1 && fail3 == 1'b1) ||
-			  (fail1 == 1'b1 && fail2 == 1'b1)) FAIL = 4'b0010;
-			  
-           if (fail1 == 1'b1 || fail2 == 1'b1 || fail3 == 1'b1 || fail4 == 1'b1 || fail5 == 1'b1 || fail6 == 1'b1) FAIL = 4'b0001;
-
+			  if (fail1 == 1'b1 || fail2 == 1'b1 || fail3 == 1'b1 || fail2 == 1'b1 || fail2 == 1'b1 || fail2 == 1'b1) begin
+				FAIL = 4'b0001;		  
+				  
+				if ((fail5 == 1'b1 && fail6 == 1'b1) ||
+				(fail4 == 1'b1 && fail6 == 1'b1) ||
+				(fail4 == 1'b1 && fail5 == 1'b1) ||
+				(fail3 == 1'b1 && fail6 == 1'b1) ||
+				(fail3 == 1'b1 && fail5 == 1'b1) ||
+				(fail3 == 1'b1 && fail4 == 1'b1) ||
+				(fail2 == 1'b1 && fail6 == 1'b1) ||
+				(fail2 == 1'b1 && fail5 == 1'b1) ||
+				(fail2 == 1'b1 && fail4 == 1'b1) ||
+				(fail2 == 1'b1 && fail3 == 1'b1) ||
+				(fail1 == 1'b1 && fail6 == 1'b1) ||
+				(fail1 == 1'b1 && fail5 == 1'b1) ||
+				(fail1 == 1'b1 && fail4 == 1'b1) ||
+				(fail1 == 1'b1 && fail3 == 1'b1) ||
+				(fail1 == 1'b1 && fail2 == 1'b1)) begin 
+					FAIL = 4'b0010;
+					
+					if ((fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+					(fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+					(fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+					(fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
+					(fail2 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+					(fail2 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+					(fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
+					(fail2 == 1'b1 && fail3 == 1'b1 && fail6 == 1'b1) ||
+					(fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1) ||
+					(fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1) ||
+					(fail1 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+					(fail1 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+					(fail1 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
+					(fail1 == 1'b1 && fail3 == 1'b1 && fail6 == 1'b1) ||
+					(fail1 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1) ||
+					(fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1)) begin 
+						FAIL = 4'b0011;
+						
+						if ((fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						(fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						(fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						(fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+						(fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
+						(fail1 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						(fail1 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						(fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+						(fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
+						(fail1 == 1'b1 && fail2 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						(fail1 == 1'b1 && fail2 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+						(fail1 == 1'b1 && fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) ||
+						(fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail6 == 1'b1) ||
+						(fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1) ||
+						(fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1)) begin 
+							FAIL = 4'b0100;
+										  
+						  if ((fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1) || 
+						  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail6 == 1'b1) ||
+						  (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						  (fail1 == 1'b1 && fail2 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						  (fail1 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) ||
+						  (fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1)) begin 
+								FAIL = 4'b0101;
+								
+								if (fail1 == 1'b1 && fail2 == 1'b1 && fail3 == 1'b1 && fail4 == 1'b1 && fail5 == 1'b1 && fail6 == 1'b1) begin 
+									FAIL = 4'b0110;
+								end
+							end
+						end	
+				   end
+				end
+			end
+			
 			  
 			  COUNT[3:0] = PASS;
            COUNT[7:4] = FAIL;
